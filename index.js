48c5a10d8de6550deb474396461ed8c34b5b1afb
Tigro <easterlingme95@gmail.com>
2022-12-26 15:27:35 -0600

    basic connections for app

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ const inquirer = require('inquirer');
 const express = require('express');
 const mysql2 = require('mysql2');
 require('dotenv').config();
+
+
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+const db = mysql2.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+);
+
+db.connect(function (err) {
+    if (err) throw err;
+    mainMenu();
+});
